b7f1a33f073b575cbeacee0ad0bcb173f4979d24
Peterqiuck <1142714348@qq.com>
2024-03-23 23:41:56 +0800

    赋值运算

---
+++ class3.3.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int a=10;
+    int send = ++a + 5;
+    std::cout<<"send=" <<send<<" a=" <<a<< std::endl;
+    a = 10;
+    //send = a-- + 5;
+    send = 5 + a--;
+    
+    std::cout << "send=" << send << " a=" << a << std::endl;
+
+    //int end = b / 3;
+    //int a = b % 4;//b除以4的余数
+    //b= b % 6;//等于上面
+    //std::cout << end<<std::endl;
+    //std::cout << b;
+}
+
