9117fc45e2f45e7dfe0c910c8805dc6084d44a02
Peterqiuck <1142714348@qq.com>
2024-03-23 23:34:54 +0800

    麟江湖

---
+++ class2.5.cpp
@@ -0,0 +1,51 @@
+﻿// class2.5.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#define dversion "v1.1"
+
+int main()
+{
+    system("color 07");
+    std::cout << "==============================================\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "               麟江湖\"控制台\"版"<<dversion;
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "                 正在加载游戏...";
+    std::cout << "\n";
+    std::cout << "                   进度[8/100]";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "        官方网站 http://edoyun.ke.qq.com\n";
+    std::cout << "\n";
+    std::cout << "==============================================\n";
+    
+    system("pause");
+    system("cls");
+
+    std::cout << "==============================================\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "请输入你的姓名：";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "               "  ;
+    std::cout << "\n";
+    std::cout << "                   ";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "        官方网站 http://edoyun.ke.qq.com\n";
+    std::cout << "\n";
+    std::cout << "==============================================\n";
+}
