9da537e9998267886e7f2710ac9ded2d85996266
Peterqiuck <1142714348@qq.com>
2024-03-24 14:57:33 +0800

    枚举

---
+++ class5.11.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    enum class weaponlv :int//定义武器为类型，默认int
+    {
+        normal = 10, neck1 = normal,//左右平级
+        high, neck2 = high,
+        rare = 2, neck3 = rare,
+        epic, neck4 = epic,
+        legend, neck5 = legend,
+        myth = 20, neck6 = myth
+
+    };
+    weaponlv equipA{ weaponlv::high };//定义初始化
+    weaponlv equipB{ weaponlv::legend };
+
+    short diff = (short)equipA - (short)equipB;//需要强制类型转换
+    std::cout << "装备品质为" << (short)equipA << char(10);//需要强制类型转换
+    std::cout << "装备差异化" << diff;
