68da70d54565564e17f0f6a865757b038c825676
Peterqiuck <1142714348@qq.com>
2024-03-26 23:14:04 +0800

    二进制1的数量

--- 算二进制1个数.c
+++ 算二进制1个数.c
@@ -1,25 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+//int main()
+//{
+//	int a = 0;
+//	int count = 0;
+//	scanf("%d", &a);
+//
+//	while(a)
+//	{
+//		if ((a & 1) == 1)
+//		{
+//			count++;
+//			a = a >> 1;
+//		}
+//		else
+//		{
+//			a = a >> 1;
+//		}
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+int Qt(int x)
+{
+	int count = 0;
+	unsigned int m = (unsigned int)x;
+	while (m)
+	{
+		if ((m & 1) == 1)
+		{
+			count++;
+			m >>= 1;
+		}
+		else m >>= 1;
+	}
+	return count;
+}
 
 int main()
 {
 	int a = 0;
-	int count = 0;
 	scanf("%d", &a);
-
-	for (int i = 0; i < 8*sizeof(a); i++)
-	{
-		if (a & 1 == 1)
-		{
-			count++;
-			a = a >> 1;
-		}
-		else
-		{
-			a = a >> 1;
-		}
-	}
-	printf("%d\n", count);
+	int ret = Qt(a);
+	printf("%d\n", ret);
 	return 0;
 }
