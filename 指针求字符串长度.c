26889d7057c1cfc85814c79c1f94a84197a2b8db
Peterqiuck <1142714348@qq.com>
2024-03-26 21:30:15 +0800

    指针求字符串长度

---
+++ 指针求字符串长度.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int the_strlen(char* str)
+{
+	char* stard = str;
+	char* end = str;
+	while (*end != '\0')
+	{
+		end++;
+	}
+	return end - stard;
+}
+
+int main()
+{
+	char arr[] = "bit";
+	int ret = 0;
+	ret = the_strlen(arr);
+	printf("%d\n", ret);
+	return 0;
+}
