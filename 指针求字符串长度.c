642fd8d3e4e064796ffef31f0fba1b2ed387fa33
Peterqiuck <1142714348@qq.com>
2024-03-27 16:23:09 +0800

    指针求字符串长度

--- 指针求字符串长度.c
+++ 指针求字符串长度.c
@@ -1,21 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-int the_strlen(char* str)
-{
-	char* stard = str;
-	char* end = str;
-	while (*end != '\0')
-	{
-		end++;
-	}
-	return end - stard;
-}
-
-int main()
-{
-	char arr[] = "bit";
-	int ret = 0;
-	ret = the_strlen(arr);
-	printf("%d\n", ret);
-	return 0;
-}
+//int the_strlen(char* str)
+//{
+//	char* stard = str;
+//	char* end = str;
+//	while (*end != '\0')
+//	{
+//		end++;
+//	}
+//	return end - stard;
+//}
+//
+//int main()
+//{
+//	char arr[] = "bit";
+//	int ret = 0;
+//	ret = the_strlen(arr);
+//	printf("%d\n", ret);
+//	return 0;
+//}
