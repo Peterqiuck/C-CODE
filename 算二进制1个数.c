8500e25c61d26c4689c621b28c169ab3456c1aae
Peterqiuck <1142714348@qq.com>
2024-03-27 16:20:58 +0800

    二进制1个数

--- 算二进制1个数.c
+++ 算二进制1个数.c
@@ -23,27 +23,42 @@
 //	return 0;
 //}
 
-int Qt(int x)
-{
-	int count = 0;
-	unsigned int m = (unsigned int)x;
-	while (m)
-	{
-		if ((m & 1) == 1)
-		{
-			count++;
-			m >>= 1;
-		}
-		else m >>= 1;
-	}
-	return count;
-}
+//强制类型转换
+//int Qt(int x)
+//{
+//	int count = 0;
+//	unsigned int m = (unsigned int)x;
+//	while (m)
+//	{
+//		if ((m & 1) == 1)
+//		{
+//			count++;
+//			m >>= 1;
+//		}
+//		else m >>= 1;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int ret = Qt(a);
+//	printf("%d\n", ret);
+//	return 0;
+//}
 
 int main()
 {
-	int a = 0;
+	int  a = 0;
+	int count = 0;
 	scanf("%d", &a);
-	int ret = Qt(a);
-	printf("%d\n", ret);
+	while (a)
+	{
+		a = a & (a - 1);
+		count++;
+	}
+	printf("%d\n", count);
 	return 0;
 }
