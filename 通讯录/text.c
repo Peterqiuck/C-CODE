76ca9e6e17fa1e097a3332fdf7a1b57795b90807
Peterqiuck <1142714348@qq.com>
2024-04-22 23:00:34 +0800

    通讯录

---
+++ 通讯录/text.c
@@ -0,0 +1,39 @@
+#include "contact.h"
+
+void menu()
+{
+	printf("**********************************************\n");
+	printf("****1.add    2.del    3.search    4.modify****\n");
+	printf("****5.show   6.sort   0.exit              ****\n");
+	printf("**********************************************\n");
+}
+int main()//如果输入的字符串数量超过限定范围，将会出现未知报错
+{
+	int op = 0;
+	struct contact un;
+	Inlicontact(&un);
+	do
+	{
+		menu();
+		printf("请选择:");
+		scanf("%d", &op);
+		switch(op)
+		{
+		case add:Addcontact(&un);//增加通讯录
+			break;
+		case del:Delcontact(&un);//删除通讯录
+			break;
+		case search:Searchcontact(&un);//查找通讯录
+			break;
+		case modify:Modifycontact(&un);//修改通讯录
+			break;
+		case show:Showcontact(&un);
+			break;
+		case sort:Sortcontact(&un);//通讯录分类排序
+			break; printf("退出通讯录");
+		default:printf("选择错误，请重新选择：");
+			break;
+		}
+	} while (op);
+	return 0;
+}
