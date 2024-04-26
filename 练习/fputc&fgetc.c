7cd24ddc1733ef920e00ea0f304e89ed4ce8348d
Peterqiuck <1142714348@qq.com>
2024-04-27 04:25:50 +0800

    字符输入输出

---
+++ 练习/fputc&fgetc.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+
+struct st
+{
+	int a;
+	float f;
+	char str[10];
+};
+//只读'r'
+//在文件text.txt里读出ab两个字符
+int main()
+{
+	FILE* pr = fopen("text.txt", "r");
+	if (pr == NULL)
+	{
+		printf("%s", strerror(errno));
+		return 0;
+	}
+	printf("%c", fgetc(pr));
+	printf("%c", fgetc(pr));
+	return 0;
+}
+
+//只写'w'
+//fputc在文件text.txt里输入ab两个字符
+//int main()
+//{
+//	FILE* pf = fopen("text.txt", "w");
+//	if (pf==NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//	fputc('a', pf);
+//	fputc('b', pf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
